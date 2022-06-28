d64ef099876dd8077e0c29d01c5d43f3bb391869
nauvalsh <nauvalsh@gmail.com>
2022-06-28 09:55:27 +0700

    ADD OUTLET

--- src/controllers/outletController.ts
+++ src/controllers/outletController.ts
@@ -1,16 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, ILike } from 'typeorm';
 import Outlet from '~/orm/entities/Outlet';
+import queryHelper from '~/utils/queryHelper';
 
 export const getOutlet = async (req: Request, res: Response, next: NextFunction) => {
   const outletRepo = getRepository(Outlet);
 
   try {
-    const [outlet, count] = await outletRepo.findAndCount();
+    const filter = {
+      nama: req.query.nama || '',
+    };
+
+    const paging = queryHelper.paging(req.query);
+
+    const [outlet, count] = await outletRepo.findAndCount({
+      take: paging.limit,
+      skip: paging.offset,
+      where: {
+        nama: ILike(`%${filter.nama}%`),
+      },
+    });
 
     const dataRes = {
       meta: {
         count,
+        limit: paging.limit,
+        offset: paging.offset,
       },
       outlet,
     };
