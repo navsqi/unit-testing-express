eb4f77f4c1425508dd0bdd68d113449e458babdd
NauvalShidqi <nauval.vendor.pasti@pegadaian.co.id>
2022-09-12 09:41:36 +0700

    Report Excel

--- src/routes/v1/reportRoute.ts
+++ src/routes/v1/reportRoute.ts
@@ -6,8 +6,12 @@ import protect from '../../middlewares/protect';
 const router = Router();
 
 router.get('/event', protect(), reportController.getReportEvent);
+router.get('/event/excel', protect(), reportController.genExcelReportEvent);
 router.get('/instansi', protect(), reportController.getReportInstansi);
+router.get('/instansi/excel', protect(), reportController.genExcelReportInstansi);
 router.get('/leads', protect(), reportController.getReportLeads);
+router.get('/leads/excel', protect(), reportController.genExcelReportLeads);
 router.get('/closing', protect(), reportController.getReportClosing);
+router.get('/closing/excel', protect(), reportController.genExcelReportClosing);
 
 export default router;
