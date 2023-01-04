34c8e80bca9916a06906649c89014b7cf7ad29e4
NauvalShidqi <nauval.vendor.pasti@pegadaian.co.id>
2023-01-04 11:29:21 +0700

    Promo microsite photos

--- src/controllers/promoMicrositeController.ts
+++ src/controllers/promoMicrositeController.ts
@@ -76,8 +76,11 @@ export const getDetailPromoMicrosite = async (req: Request, res: Response, next:
       where: { promo_microsite_id: promoMicrosite.id },
       order: { id: 'asc' },
     });
-    promoMicrosite.thumbnail = photos[0].photo;
-    promoMicrosite.photos = photos;
+
+    if (photos && photos.length > 1) {
+      promoMicrosite.thumbnail = photos[0].photo;
+      promoMicrosite.photos = photos;
+    }
 
     const promosId = promoMicrosite.promo.map((el) => el.promo_id);
     const promo = await promoRepo.find({
