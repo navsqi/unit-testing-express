cce4019a547336c7a62bbadc3cad0fe4e8335c9f
NauvalShidqi <nauval.vendor.pasti@pegadaian.co.id>
2023-01-16 15:31:54 +0700

    set token per-minute

--- src/services/tokenSvc.ts
+++ src/services/tokenSvc.ts
@@ -5,7 +5,7 @@ import User from 'orm/entities/User';
 // Create token
 export const signToken = async (user: User) => {
   // set token expires
-  const tokenExpires = dayjs().add(Number(process.env.JWT_EXPIRATION_DAY), 'day');
+  const tokenExpires = dayjs().add(Number(process.env.JWT_EXPIRATION_DAY) * 60, 'minute');
 
   const token = jwt.sign({ id: user.id, iat: dayjs().unix(), exp: tokenExpires.unix() }, process.env.JWT_SECRET_KEY, {
     algorithm: 'HS512',
