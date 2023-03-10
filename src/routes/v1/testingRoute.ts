68f392d09c2fa411b07f0b9503d858a3df546834
NauvalShidqi <nauval.vendor.pasti@pegadaian.co.id>
2023-03-10 09:15:28 +0700

    Add date updated at repo

--- src/routes/v1/testingRoute.ts
+++ src/routes/v1/testingRoute.ts
@@ -6,13 +6,13 @@ import { basicAuth } from '~/middlewares/basicAuth';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send(`Executed at ${dayjs().format('DD/MM/YYYY')}, Updated at 08/03/2023 14:15`);
+  res.send(`Executed at ${dayjs().format('DD/MM/YYYY')}, Updated at 10/03/2023 14:15`);
 });
 
 router.put('/closing-scheduler', basicAuth, async (req, res) => {
   eventHandler.emit('performBackgroundTask', 'ini data lho');
 
-  res.send(`Executed at ${dayjs().format('DD/MM/YYYY')}, Updated at 08/03/2023 14:15 | Cron dieksekusi`);
+  res.send(`Executed at ${dayjs().format('DD/MM/YYYY')}, Updated at 10/03/2023 14:15 | Cron dieksekusi`);
 });
 
 export default router;
